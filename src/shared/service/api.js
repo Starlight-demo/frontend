a5ce9b9b82ce86c2bf5d9337b0a2ee43c4a017bd
Serhii <sekret7d@gmail.com>
2024-07-06 00:33:51 +0200

    Update api.js

--- src/shared/service/api.js
+++ src/shared/service/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // const API_URL = 'http://18.194.159.42:8082/api';
-const API_URL = 'http://postgres:8082/api';
+// const API_URL = 'http://starlight-network:8082/api';
+const API_URL = 'http://localhost:8082/api'
+// const API_URL = 'http://backend:8082/api';
 
 export const publicAxiosInstance = axios.create({
   baseURL: API_URL,
